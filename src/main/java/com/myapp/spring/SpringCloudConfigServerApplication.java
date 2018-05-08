125a6bb3972b9fb84930f7c837edb7c9babbc8b3
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2018-05-08 12:55:47 +0530

    Spring Cloud Config Server added implements CommandLineRunner

---
+++ src/main/java/com/myapp/spring/SpringCloudConfigServerApplication.java
@@ -0,0 +1,25 @@
+package com.myapp.spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+// https://pastebin.com/MLmkU92v
+// added somethings
+// Added New Things
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication  implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
